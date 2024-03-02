8e4c64450df49136438eeb651c1baa07b5fbd172
parmanand007 <parmanandprajapati006@gmail.com>
2024-03-02 15:55:33 +0530

    set backend by creating stock picker and stock tracker view function

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render
-
+from yahoo_fin.stock_info import *
+from django.http import HttpResponse
+import time
+import queue
+from threading import Thread
 # Create your views here.
 
 
 def stockPicker(request):
-    return render(request,'mainapp/stockpicker.html')
+    stock_picker=tickers_nifty50()
+    print(stock_picker)
+    return render(request,'mainapp/stockpicker.html',{'stockpicker':stock_picker})
     ...
 
 def stockTracker(request):
+    stockpicker=request.GET.getlist('stockpicker')
+    print(stockpicker)
+    data={}
+    available_stocks=tickers_nifty50()
+    for i in stockpicker:
+        if i in available_stocks:
+            pass
+        else:
+            return HttpResponse("Error")
+    #we use threading  when line 25 run for single stock then our cpu is id le
+    n_threads = len(stockpicker)
+    thread_list=[]
+    que = queue.Queue()
+    start=time.time()
+    # for i in stockpicker:
+    #     details = get_quote_table(i)
+    #     data.update({i:details})
+    for i in range(n_threads):
+        thread = thread = Thread(target = lambda q, arg1: q.put({stockpicker[i]: get_quote_table(arg1)}), args = (que, stockpicker[i]))
+    end = time.time()
+    time_taken= end-start
+
+    print("time taken is",time_taken)
+    print(data)
     return render(request,"mainapp/stocktracker.html")
